10b69672cec6a9c0ea7ecfbbf3fcfcbd8e3d0757
Yecheng <1204633887@qq.com>
2018-03-14 23:36:16 -0700

    test

--- reducer.py
+++ reducer.py
@@ -9,7 +9,6 @@ import json
 
 current_word = None
 # current_count = 0
-word = None
 
 #output_result_obj {"keyvalue": stats}
 output_result_obj = {}
@@ -28,17 +27,17 @@ for line in sys.stdin:
 
     if 'word' not in obj or ('body_loc' not in obj and 'title_loc' not in obj):
         continue
-    current_word = obj['word']
+    if not current_word:
+        current_word = obj['word']
+        output_result_obj[current_word] = stats
+    word = obj['word']
 
     # parse the input we got from mapper.py
     # word, count = line.split('\t', 1)
 
     # this IF-switch only works because Hadoop sorts map output
     # by key (here: word) before it is passed to the reducer
-    if len(output_result_obj.keys()) == 0:
-        output_result_obj[current_word] = stats
     if current_word == word:
-        # current_count += count
         if 'body_loc' in obj:
             output_result_obj['body']['frequency'] += 1
             output_result_obj['body']['location'].append(obj['body_loc'])
@@ -46,8 +45,11 @@ for line in sys.stdin:
             output_result_obj['title']['frequency'] += 1
             output_result_obj['title']['location'].append(obj['title_loc'])
     else:
-        if current_word:
-            pass
+        current_word = word
+        output_result_obj = {}
+        output_result_obj[word] = stats
+        print json.dumps(output_result_obj)
+        print "\n"
             # print current_word
             # write result to STDOUT
             # print '%s\t%s' % (current_word, current_count)
