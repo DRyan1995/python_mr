43f2e0415b0efb9ff20c08c2dec04a4ad3a6ff1b
Yecheng <1204633887@qq.com>
2018-03-14 22:34:10 -0700

    test

--- mapper.py
+++ mapper.py
@@ -1,19 +1,50 @@
 #!/usr/bin/env python
 """mapper.py"""
-
 import sys
+import re
+import json
+
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    # line = line.strip()
+    try:
+        obj = json.loads(line)
+    except ValueError as e:
+        continue
+
     # split the line into words
-    words = line.split()
-    # increase counters
-    for word in words:
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
-        print '%s\t%s' % (word, 1)
+    if 'url' not in obj:
+        continue
+
+    if 'body' in obj:
+        loc = -1
+        words = obj['body'].split(' ')
+        for word in words:
+            loc += 1
+            word = word.decode("utf8")
+            word = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+".decode("utf8"), "".decode("utf8"),word)
+            if len(word) == 0:
+                continue
+            print json.dumps({"word": word, "body_loc": loc})
+    if 'title' in obj:
+        loc = -1
+        words = obj['title'].split(' ')
+        for word in words:
+            loc += 1
+            word = word.decode("utf8")
+            word = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+".decode("utf8"), "".decode("utf8"),word)
+            if len(word) == 0:
+                continue
+            print json.dumps({"word": word, "title_loc": loc})
+    #
+    # words = line.split()
+    # # increase counters
+    # for word in words:
+    #     # write the results to STDOUT (standard output);
+    #     # what we output here will be the input for the
+    #     # Reduce step, i.e. the input for reducer.py
+    #     #
+    #     # tab-delimited; the trivial word count is 1
+    #     print '%s\t%s' % (word, 1)
--- reducer.py
+++ reducer.py
@@ -3,38 +3,54 @@
 
 from operator import itemgetter
 import sys
+import json
 
 current_word = None
-current_count = 0
+# current_count = 0
 word = None
+
+#output_result_obj {"keyvalue": stats}
+output_result_obj = {}
+stats = {'body':{'frequency': 0, 'location':[]}, 'title':{'frequency': 0, 'location':[]}} #{"body/title":{frequency:, "location":[]}}
 
 # input comes from STDIN
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    try:
+        obj = json.loads(line) # obj here {"word": word, "body_loc/title_loc": }
+    except ValueError as e:
+        continue
+
+    if 'word' not in obj or ('body_loc' not in obj and 'title_loc' not in obj):
+        continue
+    current_word = obj['word']
 
     # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
-
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
+    # word, count = line.split('\t', 1)
 
     # this IF-switch only works because Hadoop sorts map output
     # by key (here: word) before it is passed to the reducer
+    if len(output_result_obj.keys()) == 0:
+        output_result_obj[current_word] = stats
     if current_word == word:
-        current_count += count
+        # current_count += count
+        if 'body_loc' in obj:
+            output_result_obj['body']['frequency'] += 1
+            output_result_obj['body']['location'].append(body_loc)
+        if 'title_loc' in obj:
+            output_result_obj['title']['frequency'] += 1
+            output_result_obj['title']['location'].append(body_loc)
+
     else:
         if current_word:
+            pass
             # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
+            # print '%s\t%s' % (current_word, current_count)
+        # current_count = count
         current_word = word
 
 # do not forget to output the last word if needed!
 if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
+    # print '%s\t%s' % (current_word, current_count)
+    print json.dumps(output_result_obj)
